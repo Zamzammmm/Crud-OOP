6b32becd79c9e922fc13f2776ae384d9c88b7b7e
zamzammmm <zamzamzuba2017@gmail.com>
2019-10-16 08:05:14 +0700

    Crud OOP + PHPMyAdmin

--- Siswa/index.php
+++ Siswa/index.php
@@ -30,8 +30,8 @@ include '../database.php';
             <td><?php echo $data['nis']; ?></td>
             <td><?php echo $data['nama']; ?></td>
             <td><?php echo $data['alamat']; ?></td>
-            <td><a href="show.php?id=<?php echo $data['id']; ?>&aksi=show">Show</a></td>
-            <td><a href="edit.php?id=<?php echo $data['id']; ?>&aksi=edit">Edit</a></td>
+            <td><a href="show.php?id=<?php echo $data['id']; ?>">Show</a></td>
+            <td><a href="edit.php?id=<?php echo $data['id']; ?>">Edit</a></td>
             <td><a href="proses.php?id=<?php echo $data['id']; ?>&aksi=delete">Delete</a></td>
         </tr>
         <?php }?>
